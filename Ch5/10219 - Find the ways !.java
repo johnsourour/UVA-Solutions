3e746175c9ebcb390138542d0b58110b21e3c264
John Sourour <johnsourour@aucegypt.edu>
2017-07-25 19:37:28 +0200

    Create 10219 - Find the ways !.java

---
+++ Ch5/10219 - Find the ways !.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+class Main
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		BigInteger n,k,ans;
+		while(sc.hasNext())
+		{
+			n = sc.nextBigInteger();
+			k = sc.nextBigInteger();
+			System.out.println(cnt(n,k));
+		}
+	
+	}
+	static int cnt(BigInteger n, BigInteger k)
+	{
+		BigInteger prod = BigInteger.ONE;
+		k=k.add(BigInteger.ONE);
+		for (BigInteger i = BigInteger.ONE; !i.equals(k); i=i.add(BigInteger.ONE))
+			prod = prod.multiply(n.subtract(i).add(BigInteger.ONE));
+		for (BigInteger i = BigInteger.valueOf(2); !i.equals(k); i=i.add(BigInteger.ONE))
+			prod =prod.divide(i);
+		return prod.toString().length();
+	}
+	
+}

2f852613205624ea300d83d59107cf1e0e853119
John Sourour <johnsourour@aucegypt.edu>
2018-10-09 22:41:41 +0200

    More

--- Ch8/711 - Dividing up.cpp
+++ Ch8/711 - Dividing up.cpp
@@ -4,12 +4,12 @@ using namespace std;
 typedef long long ll;
 
 bool can;
-ll target;
 ll marbles[6];
 bool solve(int i, ll rem, int depth){
+	
+	if(depth>marbles[i] || i==6)return false;
 	if(rem==0)return true;
-	if(i==6 || depth>marbles[i])return 0;
-	return solve(i+1, rem-i+1,0) || solve(i+1, rem,0) || solve(i, rem-i+1,depth+1);
+	return solve(i+1, rem,0) || solve(i, rem-i-1,depth+1);
 }
 int main(){
 	int K=0;
@@ -20,23 +20,22 @@ int main(){
 			tot+=(marbles[i]*(i+1));
 		}
 		if(tot==0)break;
-		if(K)printf("\n");
 		printf("Collection #%d:\n",++K);
-		
-		for(int i=0;i<6;i++){
-			if(marbles[i]>12){
-				int rem = marbles[i]%2;
+		if(tot%2==0)
+		for(ll i=0;i<6;i++){
+			if(marbles[i]>13){
+				int rem = marbles[i]&1;
 				tot-=(marbles[i]-12-rem)*(i+1);
 				marbles[i]=rem+12;
 			}
 			
 		}
-		target = tot/2;
+		ll target = tot/2;
 		if(tot%2==0 && solve(0,target,0)){
-			printf("Can be divided.\n");
+			printf("Can be divided.\n\n");
 		}
 		else{
-			printf("Can't be divided.\n");
+			printf("Can't be divided.\n\n");
 		}
 	}
 	return 0;
